7af185712ba7ca022a87c1722281b37cb33ff580
Murilo Sandiego <murilo@murilo.dev>
2024-06-17 17:51:44 -0300

    Remove Bear Notes and Magnet from mas install list
    This commit removes the commands to install Bear Notes and Magnet from the Mac App Store (mas) installation script. These apps are no longer needed/supported, thus the corresponding lines of code were deleted.

--- mas/mas.sh
+++ mas/mas.sh
@@ -1,9 +1,6 @@
 echo
 echo "--------- 💻 APP STORE APPS ---------"
 
-# Bear Notes
-mas install 1091189122
-
 # Spark Mail Client
 mas install 6445813049
 
@@ -15,9 +12,6 @@ mas install 497799835
 
 # RocketSim
 mas install 1504940162
-
-# Magnet
-mas install 441258766
 
 # Swift Playgrounds
 mas install 1496833156
