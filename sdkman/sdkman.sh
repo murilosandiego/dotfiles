d0de92bad69f3b52c55b29033d46fdba2f7936ca
Murilo Sandiego <murilo@murilo.dev>
2024-06-18 00:31:51 -0300

    feat: Update SDKMAN installation method and add Java installation
    The SDKMAN installation method has been updated to use /bin/bash directly, providing a safe and secure script execution. Additionally, a step to install Java using SDKMAN is now included, making the setup process easier for users.

--- sdkman/sdkman.sh
+++ sdkman/sdkman.sh
@@ -2,10 +2,14 @@ if [[ ! -d $HOME/.sdkman ]]; then
   echo
   echo "--------- ⚙️ SDKMAN ---------"
   echo "* Installing sdkman"
-  curl --silent "https://get.sdkman.io" | bash > /dev/null
+
+  /bin/bash -c "$(curl -fsSL https://get.sdkman.io)"
 
   export SDKMAN_DIR="$HOME/.sdkman"
   [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+  # Install java
+  sdk install java
 
   echo "✅ 'SDKMAN' installed successfully!"
 fi
